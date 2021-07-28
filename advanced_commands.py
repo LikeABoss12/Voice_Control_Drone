fb60f5ff5e20c736e311344018754cecc922d927
Sanath <sanathmurali12@gmail.com>
2021-07-28 09:52:43 -0500

    Amended file (made changes to return values)

---
+++ advanced_commands.py
@@ -0,0 +1,18 @@
+from functions_simple_commands import *
+my_drone = tello.Tello()
+mic = sr.Microphone()
+recognizer = sr.Recognizer()
+my_drone.connect()
+print(my_drone.get_battery())
+print(sr.__version__)
+
+while my_drone.connect() is True:
+    result = voice_command()
+    statement = execute_command(result)
+
+    if statement == True:
+        continue
+
+    else:
+        print('Loop Failed')
+        break
